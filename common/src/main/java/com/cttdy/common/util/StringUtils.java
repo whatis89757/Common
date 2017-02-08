a48de22dd2f8f052a411b27f4f03e77fde767c17
whatis89757 <whatis89757@163.com>
2017-02-08 08:45:25 +0800

    common util

---
+++ common/src/main/java/com/cttdy/common/util/StringUtils.java
@@ -0,0 +1,361 @@
+package com.cttdy.common.util;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * String Utils
+ * 
+ * @author <a href="http://www.trinea.cn" target="_blank">Trinea</a> 2011-7-22
+ */
+public class StringUtils {
+
+    private StringUtils() {
+        throw new AssertionError();
+    }
+
+    /**
+     * is null or its length is 0 or it is made by space
+     * 
+     * <pre>
+     * isBlank(null) = true;
+     * isBlank(&quot;&quot;) = true;
+     * isBlank(&quot;  &quot;) = true;
+     * isBlank(&quot;a&quot;) = false;
+     * isBlank(&quot;a &quot;) = false;
+     * isBlank(&quot; a&quot;) = false;
+     * isBlank(&quot;a b&quot;) = false;
+     * </pre>
+     * 
+     * @param str
+     * @return if string is null or its size is 0 or it is made by space, return true, else return false.
+     */
+    public static boolean isBlank(String str) {
+        return (str == null || str.trim().length() == 0);
+    }
+
+    /**
+     * is null or its length is 0
+     * 
+     * <pre>
+     * isEmpty(null) = true;
+     * isEmpty(&quot;&quot;) = true;
+     * isEmpty(&quot;  &quot;) = false;
+     * </pre>
+     * 
+     * @param str
+     * @return if string is null or its size is 0, return true, else return false.
+     */
+    public static boolean isEmpty(CharSequence str) {
+        return (str == null || str.length() == 0);
+    }
+
+    /**
+     * compare two string
+     * 
+     * @param actual
+     * @param expected
+     * @return
+     * @see ObjectUtils#isEquals(Object, Object)
+     */
+    public static boolean isEquals(String actual, String expected) {
+        return ObjectUtils.isEquals(actual, expected);
+    }
+
+    /**
+     * get length of CharSequence
+     * 
+     * <pre>
+     * length(null) = 0;
+     * length(\"\") = 0;
+     * length(\"abc\") = 3;
+     * </pre>
+     * 
+     * @param str
+     * @return if str is null or empty, return 0, else return {@link CharSequence#length()}.
+     */
+    public static int length(CharSequence str) {
+        return str == null ? 0 : str.length();
+    }
+
+    /**
+     * null Object to empty string
+     * 
+     * <pre>
+     * nullStrToEmpty(null) = &quot;&quot;;
+     * nullStrToEmpty(&quot;&quot;) = &quot;&quot;;
+     * nullStrToEmpty(&quot;aa&quot;) = &quot;aa&quot;;
+     * </pre>
+     * 
+     * @param str
+     * @return
+     */
+    public static String nullStrToEmpty(Object str) {
+        return (str == null ? "" : (str instanceof String ? (String)str : str.toString()));
+    }
+
+    /**
+     * capitalize first letter
+     * 
+     * <pre>
+     * capitalizeFirstLetter(null)     =   null;
+     * capitalizeFirstLetter("")       =   "";
+     * capitalizeFirstLetter("2ab")    =   "2ab"
+     * capitalizeFirstLetter("a")      =   "A"
+     * capitalizeFirstLetter("ab")     =   "Ab"
+     * capitalizeFirstLetter("Abc")    =   "Abc"
+     * </pre>
+     * 
+     * @param str
+     * @return
+     */
+    public static String capitalizeFirstLetter(String str) {
+        if (isEmpty(str)) {
+            return str;
+        }
+
+        char c = str.charAt(0);
+        return (!Character.isLetter(c) || Character.isUpperCase(c)) ? str : new StringBuilder(str.length())
+                .append(Character.toUpperCase(c)).append(str.substring(1)).toString();
+    }
+
+    /**
+     * encoded in utf-8
+     * 
+     * <pre>
+     * utf8Encode(null)        =   null
+     * utf8Encode("")          =   "";
+     * utf8Encode("aa")        =   "aa";
+     * utf8Encode("啊啊啊啊")   = "%E5%95%8A%E5%95%8A%E5%95%8A%E5%95%8A";
+     * </pre>
+     * 
+     * @param str
+     * @return
+     * @throws UnsupportedEncodingException if an error occurs
+     */
+    public static String utf8Encode(String str) {
+        if (!isEmpty(str) && str.getBytes().length != str.length()) {
+            try {
+                return URLEncoder.encode(str, "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                throw new RuntimeException("UnsupportedEncodingException occurred. ", e);
+            }
+        }
+        return str;
+    }
+
+    /**
+     * encoded in utf-8, if exception, return defultReturn
+     * 
+     * @param str
+     * @param defultReturn
+     * @return
+     */
+    public static String utf8Encode(String str, String defultReturn) {
+        if (!isEmpty(str) && str.getBytes().length != str.length()) {
+            try {
+                return URLEncoder.encode(str, "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                return defultReturn;
+            }
+        }
+        return str;
+    }
+
+    /**
+     * get innerHtml from href
+     * 
+     * <pre>
+     * getHrefInnerHtml(null)                                  = ""
+     * getHrefInnerHtml("")                                    = ""
+     * getHrefInnerHtml("mp3")                                 = "mp3";
+     * getHrefInnerHtml("&lt;a innerHtml&lt;/a&gt;")                    = "&lt;a innerHtml&lt;/a&gt;";
+     * getHrefInnerHtml("&lt;a&gt;innerHtml&lt;/a&gt;")                    = "innerHtml";
+     * getHrefInnerHtml("&lt;a&lt;a&gt;innerHtml&lt;/a&gt;")                    = "innerHtml";
+     * getHrefInnerHtml("&lt;a href="baidu.com"&gt;innerHtml&lt;/a&gt;")               = "innerHtml";
+     * getHrefInnerHtml("&lt;a href="baidu.com" title="baidu"&gt;innerHtml&lt;/a&gt;") = "innerHtml";
+     * getHrefInnerHtml("   &lt;a&gt;innerHtml&lt;/a&gt;  ")                           = "innerHtml";
+     * getHrefInnerHtml("&lt;a&gt;innerHtml&lt;/a&gt;&lt;/a&gt;")                      = "innerHtml";
+     * getHrefInnerHtml("jack&lt;a&gt;innerHtml&lt;/a&gt;&lt;/a&gt;")                  = "innerHtml";
+     * getHrefInnerHtml("&lt;a&gt;innerHtml1&lt;/a&gt;&lt;a&gt;innerHtml2&lt;/a&gt;")        = "innerHtml2";
+     * </pre>
+     * 
+     * @param href
+     * @return <ul>
+     *         <li>if href is null, return ""</li>
+     *         <li>if not match regx, return source</li>
+     *         <li>return the last string that match regx</li>
+     *         </ul>
+     */
+    public static String getHrefInnerHtml(String href) {
+        if (isEmpty(href)) {
+            return "";
+        }
+
+        String hrefReg = ".*<[\\s]*a[\\s]*.*>(.+?)<[\\s]*/a[\\s]*>.*";
+        Pattern hrefPattern = Pattern.compile(hrefReg, Pattern.CASE_INSENSITIVE);
+        Matcher hrefMatcher = hrefPattern.matcher(href);
+        if (hrefMatcher.matches()) {
+            return hrefMatcher.group(1);
+        }
+        return href;
+    }
+
+/**
+     * process special char in html
+     * 
+     * <pre>
+     * htmlEscapeCharsToString(null) = null;
+     * htmlEscapeCharsToString("") = "";
+     * htmlEscapeCharsToString("mp3") = "mp3";
+     * htmlEscapeCharsToString("mp3&lt;") = "mp3<";
+     * htmlEscapeCharsToString("mp3&gt;") = "mp3\>";
+     * htmlEscapeCharsToString("mp3&amp;mp4") = "mp3&mp4";
+     * htmlEscapeCharsToString("mp3&quot;mp4") = "mp3\"mp4";
+     * htmlEscapeCharsToString("mp3&lt;&gt;&amp;&quot;mp4") = "mp3\<\>&\"mp4";
+     * </pre>
+     * 
+     * @param source
+     * @return
+     */
+    public static String htmlEscapeCharsToString(String source) {
+        return StringUtils.isEmpty(source) ? source : source.replaceAll("&lt;", "<").replaceAll("&gt;", ">")
+                .replaceAll("&amp;", "&").replaceAll("&quot;", "\"");
+    }
+
+    /**
+     * transform half width char to full width char
+     * 
+     * <pre>
+     * fullWidthToHalfWidth(null) = null;
+     * fullWidthToHalfWidth("") = "";
+     * fullWidthToHalfWidth(new String(new char[] {12288})) = " ";
+     * fullWidthToHalfWidth("！＂＃＄％＆) = "!\"#$%&";
+     * </pre>
+     * 
+     * @param s
+     * @return
+     */
+    public static String fullWidthToHalfWidth(String s) {
+        if (isEmpty(s)) {
+            return s;
+        }
+
+        char[] source = s.toCharArray();
+        for (int i = 0; i < source.length; i++) {
+            if (source[i] == 12288) {
+                source[i] = ' ';
+                // } else if (source[i] == 12290) {
+                // source[i] = '.';
+            } else if (source[i] >= 65281 && source[i] <= 65374) {
+                source[i] = (char)(source[i] - 65248);
+            } else {
+                source[i] = source[i];
+            }
+        }
+        return new String(source);
+    }
+
+    /**
+     * transform full width char to half width char
+     * 
+     * <pre>
+     * halfWidthToFullWidth(null) = null;
+     * halfWidthToFullWidth("") = "";
+     * halfWidthToFullWidth(" ") = new String(new char[] {12288});
+     * halfWidthToFullWidth("!\"#$%&) = "！＂＃＄％＆";
+     * </pre>
+     * 
+     * @param s
+     * @return
+     */
+    public static String halfWidthToFullWidth(String s) {
+        if (isEmpty(s)) {
+            return s;
+        }
+
+        char[] source = s.toCharArray();
+        for (int i = 0; i < source.length; i++) {
+            if (source[i] == ' ') {
+                source[i] = (char)12288;
+                // } else if (source[i] == '.') {
+                // source[i] = (char)12290;
+            } else if (source[i] >= 33 && source[i] <= 126) {
+                source[i] = (char)(source[i] + 65248);
+            } else {
+                source[i] = source[i];
+            }
+        }
+        return new String(source);
+    }
+    /*
+     * Unicode to UTF-8
+     */
+    public static String decodeUnicode(String theString) {  
+        char aChar;  
+        int len = theString.length();  
+        StringBuffer outBuffer = new StringBuffer(len);  
+        for (int x = 0; x < len;) {  
+            aChar = theString.charAt(x++);  
+            if (aChar == '\\') {  
+                aChar = theString.charAt(x++);  
+                if (aChar == 'u') {  
+                    // Read the xxxx  
+                    int value = 0;  
+                    for (int i = 0; i < 4; i++) {  
+                        aChar = theString.charAt(x++);  
+                        switch (aChar) {  
+                        case '0':  
+                        case '1':  
+                        case '2':  
+                        case '3':  
+                        case '4':  
+                        case '5':  
+                        case '6':  
+                        case '7':  
+                        case '8':  
+                        case '9':  
+                            value = (value << 4) + aChar - '0';  
+                            break;  
+                        case 'a':  
+                        case 'b':  
+                        case 'c':  
+                        case 'd':  
+                        case 'e':  
+                        case 'f':  
+                            value = (value << 4) + 10 + aChar - 'a';  
+                            break;  
+                        case 'A':  
+                        case 'B':  
+                        case 'C':  
+                        case 'D':  
+                        case 'E':  
+                        case 'F':  
+                            value = (value << 4) + 10 + aChar - 'A';  
+                            break;  
+                        default:  
+                            throw new IllegalArgumentException(  
+                                    "Malformed   \\uxxxx   encoding.");  
+                        }  
+      
+                    }  
+                    outBuffer.append((char) value);  
+                } else {  
+                    if (aChar == 't')  
+                        aChar = '\t';  
+                    else if (aChar == 'r')  
+                        aChar = '\r';  
+                    else if (aChar == 'n')  
+                        aChar = '\n';  
+                    else if (aChar == 'f')  
+                        aChar = '\f';  
+                    outBuffer.append(aChar);  
+                }  
+            } else  
+                outBuffer.append(aChar);  
+        }  
+        return outBuffer.toString();  
+    }
+}
